a819ccc07b150b3f8f3a5eab7ff519845a9caa99
Kiquzo <167250383+Kiquzo@users.noreply.github.com>
2024-05-02 16:03:32 +0300

    [ADD] функция окраса текста навигации

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(function(){
-    let hideShow =$('h1, .call-to-ass').hide(1200);
-    $('h1, .call-to-ass').show(1200);
+    let hideShow =$('h1, .call-to-ass').hide(2500);
+    $('h1, .call-to-ass').show(2500);
+});
+
+$(document).ready(function() {
+    $('.colors').on({
+        mouseenter: function() {
+            $(this).css("color", "#F0B479");
+            
+        },
+        mouseleave: function() {
+            $(this).css("color", "white");
+        }
+    });
 });
